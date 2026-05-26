1b2644d1959c0ab8ceb6b989562791b98fcdc00a
Derick167 <derick.decastro@student.otahuhucollege.school.nz>
2026-05-26 12:14:42 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function loadPage(page){
+
+
+    fetch(page)
+    .then(Response => Response.text()) 
+    .then(data => {
+    document.getElementById("content").innerHTML = data;
+    
+
+       });
+    
+    }
+
+    
